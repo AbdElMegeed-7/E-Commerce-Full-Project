1db2c3062e55d9ea9b61ce57eba2676b8d42bca6
AbdElMegeed <AbdElMegeedX7@gmail.com>
2022-08-14 17:10:56 +0400

    Cart & CartItem models

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'category.apps.CategoryConfig',
     'accounts.apps.AccountsConfig',
     'store.apps.StoreConfig',
-    'cart.apps.Cart`Config',
+    'cart.apps.CartConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
